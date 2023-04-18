d7b51e151af396a36f13415447a27f4b5752d003
restas13 <72560360+restas13@users.noreply.github.com>
2023-04-18 16:13:26 -0700

    inquirer gets information from the user and is able to add it to the readme, readme is partially formatted

--- Assets/index.js
+++ Assets/index.js
@@ -1,2 +1,71 @@
+const { error } = require('console');
 const fs = require('fs');
 const inquirer = require('inquirer');
+
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'What is the name of your project?',
+        name: 'Project',
+    },
+    {
+        type: 'input',
+        message: 'Enter a brief description of the project:',
+        name: 'Description',
+    },
+    {
+        type: 'input',
+        message: 'What code should be used to install this project?',
+        name: 'Installation',
+    },
+    {
+        type: 'input',
+        message: 'How should this project be run?',
+        name: 'Usage',
+    },
+    {
+        type: 'select',
+        message: 'What license is used for this project?',
+        choices: ['license1', 'license2', 'license3', 'none'],
+        name: 'license',
+    },
+    {
+        type: 'input',
+        message: 'What are some guidelines for contributing?',
+        name: 'Contribution',
+    },
+    {
+        type: 'input',
+        message: 'What email should be used to contact you for questions?',
+        name: 'email',
+    },
+    {
+        type: 'input',
+        message: 'Please enter your github profile link:',
+        name: 'github',
+    },
+]).then((response) => {
+    fs.writeFile('readme.md', 
+    `# ${response.Project}
+>## Table of Contents
+* [Description]()
+* [Installation]()
+* [Usage]()
+* [Contributing]()
+* [Tests]()
+* [Questions]()
+>## Description
+${response.Description}
+>## Installation
+    ${response.Installation}
+>## Usage
+    ${response.Usage}
+>## Contributing
+${response.Contribution}
+>## Tests
+
+>## Questions`
+    , (error) => {
+        error ? console.log(error) : console.log('your readme has been written!');
+    })
+})
---
+++ Assets/readme.md
@@ -0,0 +1,19 @@
+# readme gen
+>## Table of Contents
+* [Description]()
+* [Installation]()
+* [Usage]()
+* [Contributing]()
+* [Tests]()
+* [Questions]()
+>## Description
+This project generates professional readme's based on the user's input. The project uses node, file systems, and the inquirer package to generate a readme file with the user's 
+>## Installation
+    npm init
+>## Usage
+    node index,js
+>## Contributing
+no gay
+>## Tests
+
+>## Questions
